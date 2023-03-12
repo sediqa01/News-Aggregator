792565abb047f0bd7f2f05dc47011ee952ec09bd
Sediqa <sadat.hadid2000@gmail.com>
2023-03-12 10:49:31 +0000

    Add footer in base.html

--- news/views.py
+++ news/views.py
@@ -6,6 +6,7 @@ from .models import Article
 class NewsList(generic.ListView):
     model = Article
 
-    queryset = Article.objects.filter(published_status=1).order_by("-created_on")
+    queryset = Article.objects.filter(
+        published_status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 3
