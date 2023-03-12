302953fb82824a86f6d6db1e3b1be94c8b8b331f
Sediqa <sadat.hadid2000@gmail.com>
2023-03-12 09:32:41 +0000

    Create first clasee base view

--- news/views.py
+++ news/views.py
@@ -3,5 +3,9 @@ from django.views import generic
 from .models import Article
 
 
-def base(request):
-    return render(request, 'base.html', {})
+class NewsList(generic.ListView):
+    model = Article
+
+    queryset = Article.objects.filter(published_status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 3
