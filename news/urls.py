f9260e86e64709489111af663f180aa6336bee4a
Sediqa <sadat.hadid2000@gmail.com>
2023-03-14 16:57:05 +0000

    Create the news detail class view

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.NewsList.as_view(), name="home"),
+    path('<slug:slug>/', views.NewsDetail.as_view(), name='article'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -8,5 +8,24 @@ class NewsList(generic.ListView):
 
     queryset = Article.objects.filter(
         published_status=1).order_by("-created_on")
-    template_name = "article.html"
+    template_name = "base.html"
     paginate_by = 3
+
+
+class NewsDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        news = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "article.html",
+            {
+                "news": news,
+                "liked": liked
+            },
+        )
