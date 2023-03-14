a7ca6bc2a33b2d2327d3743d61a018fc9a5a3b50
Sediqa <sadat.hadid2000@gmail.com>
2023-03-14 19:28:20 +0000

    Add style to dislay date feature

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,46 @@
 function displayDate() {
+
     let today = new Date();
-  
-    
+    // return number
+    let dayName = today.getDay(),
+      dayNum = today.getDate(),
+      month = today.getMonth(),
+      year = today.getFullYear();
+
+const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const Weeksday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    // the html element
+    const id = ["day", "daynum", "month", "year"];
+
+    // return value array with number as a index
+    const val = [Weeksday[dayName], dayNum, months[month], year];
+    for (let i = 0; i < id.length; i++) {
+      document.getElementById(id[i]).firstChild.nodeValue = val[i];
+    }
   }
+
+  displayDate();
   
   
   
