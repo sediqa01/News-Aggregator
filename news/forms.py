656eb28d5002aa6b9154002a26f3675fc6f75b33
Sediqa <sadat.hadid2000@gmail.com>
2023-03-24 23:24:21 +0000

    Create CommentsForm class

--- news/forms.py
+++ news/forms.py
@@ -1,2 +1,13 @@
 from django import forms
 from news.models import Comment
+
+
+class CommentsForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': '4',
+    }))
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
+
