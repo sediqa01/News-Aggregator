771f5afc06e96d211750a517b15ca945f70b92b2
Sediqa <sadat.hadid2000@gmail.com>
2023-03-25 18:28:34 +0000

    Add post method for comments

--- news/views.py
+++ news/views.py
@@ -28,6 +28,39 @@ class NewsDetail(View):
                 "article": news,
                 "comments": comments,
                 "liked": liked,
-                "comment_form": CommentsForm()
+                "form": CommentsForm()
             },
         )
+
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Article.objects.filter(status=1)
+        news = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if news.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            form.instance.user = request.user
+            comment = form.save(commit=False)
+            comment.news = news
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+        request,
+        "article.html",
+        {
+            "article": news,
+            "comments": comments,
+            "commented": True,
+            "form": comment_form,
+            "liked": liked
+        },
+    )
+
+        
