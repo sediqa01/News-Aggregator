8e0ab89370f48871410491d49ec9cfdb70f1e672
Sediqa <sadat.hadid2000@gmail.com>
2023-03-26 20:30:44 +0000

    Modify temlate, add like content

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.NewsList.as_view(), name="home"),
     path('<slug:slug>/', views.NewsDetail.as_view(), name='article'),
+    path('like/<slug:slug>', views.Like.as_view(), name='news_like'),
 ]
