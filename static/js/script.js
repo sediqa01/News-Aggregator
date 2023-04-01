d7607bc6dae9144aa7c3bdd7cb580a2bddd0ca79
Sediqa <sadat.hadid2000@gmail.com>
2023-04-01 16:54:23 +0000

    Add django flash message

--- static/js/script.js
+++ static/js/script.js
@@ -41,6 +41,14 @@ const months = [
   }
 
   displayDate();
+
+  
+// set time for disapearing django-flash-message
+  setTimeout(function () {
+    let messages = document.getElementById('flash-msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 1500);
   
   
   
